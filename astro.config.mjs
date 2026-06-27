017d1469f921f9b814399a5300d4f369a56b7dfb
Yu-yu0202 <git@yu-yu0202.com>
2026-06-27 11:28:52 +0900

    fix(config): switch output mode from server to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { defineConfig, fontProviders } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
+	output: "static",
 
 	adapter: node({
 		mode: "standalone",
