9b21f390c636907f8028b99e5e7b2742e7073bc1
Yu-yu0202 <git@yu-yu0202.com>
2026-06-27 15:10:28 +0900

    fix(scripts): make seeding script use environment variables for username and password

--- scripts/database/seed.ts
+++ scripts/database/seed.ts
@@ -2,12 +2,12 @@ import { db } from "@database";
 import { users } from "@database/schema";
 import { hash } from "@node-rs/argon2";
 
-const passwordHash = await hash(Bun.env["PASSWORD"]!, {
+const passwordHash = await hash(Bun.env.PASSWORD!, {
 	algorithm: 2,
 });
 
 await db.insert(users).values({
-	username: "admin",
+	username: Bun.env.USERNAME || "admin",
 	displayName: "管理者",
 	passwordHash: passwordHash,
 });
